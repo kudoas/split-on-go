88621af9c0990976088463ca3d53e3f581ec7ae8
Daichi Kudo <sakanactor624@gmail.com>
2023-09-14 00:33:10 +0900

    fix: 変数名に略称を使わない

--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 )
 
 func main() {
-	bytePtr := flag.Int("b", 0, "-b: Create split files byte_count bytes in length.")
-	linePtr := flag.Int("l", 0, "-l: Create split files line_count lines in length.")
-	chunkPtr := flag.Int("n", 0, "-n: Split file into chunk_count smaller files.")
+	byteOption := flag.Int("b", 0, "-b: Create split files byte_count bytes in length.")
+	lineOption := flag.Int("l", 0, "-l: Create split files line_count lines in length.")
+	chunkOption := flag.Int("n", 0, "-n: Split file into chunk_count smaller files.")
 	flag.Parse()
 	tailArgs := flag.Args()
 
-	cliOptions := NewCLIOption(*bytePtr, *linePtr, *chunkPtr)
+	cliOptions := NewCLIOption(*byteOption, *lineOption, *chunkOption)
 	err := cliOptions.Handle(os.Args[1:], tailArgs)
 	if err != nil {
 		fmt.Println(err)
